e5220d70f7d231dcd2065056858f35a782666d15
florent11 <48155706+florent11@users.noreply.github.com>
2019-12-05 16:49:52 +0100

    Ajout de nveaux scrypts de migration

---
+++ src/Migrations/Version20191020181550.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191020181550 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE annonces ADD marque VARCHAR(100) NOT NULL, ADD modele VARCHAR(100) NOT NULL, ADD annee_modele INT NOT NULL, ADD kilometre INT NOT NULL, ADD carburant VARCHAR(100) NOT NULL, ADD boite_de_vitesse VARCHAR(100) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE annonces DROP marque, DROP modele, DROP annee_modele, DROP kilometre, DROP carburant, DROP boite_de_vitesse');
+    }
+}
