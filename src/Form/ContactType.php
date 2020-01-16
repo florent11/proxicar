8f674eb1eff76c35f0c8047cbef029f5c9b0fec6
florent11 <florentvila@hotmail.fr>
2020-01-16 16:39:25 +0100

    Retrait  d'une ligne inutile

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -10,7 +10,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
