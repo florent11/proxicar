a766e02e08f3e31aba69df8a12dbd06c75a487d8
florent11 <florentvila@hotmail.fr>
2020-04-05 13:32:14 +0200

    Désinstallation de VichUploader (composer remove)

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
