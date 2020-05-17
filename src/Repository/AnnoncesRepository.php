d1bc9b3f928df463a6e9c86a6a163b4118ebabae
florent11 <florentvila@hotmail.fr>
2020-05-17 16:43:13 +0200

    Ajout d'une requête SQL dans AnnoncesRepo pour formulaire de recherche.

--- src/Repository/AnnoncesRepository.php
+++ src/Repository/AnnoncesRepository.php
@@ -25,66 +25,77 @@ class AnnoncesRepository extends ServiceEntityRepository
         if ($resultSearchData['mots_cles']) {
             $queryBuilder
             ->andWhere('a.ann_titre like :annTitre')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('annTitre', '%'.$resultSearchData['mots_cles'].'%')
             ;
         }
         if ($resultSearchData['prix_min']) {
             $queryBuilder
             ->andWhere('a.ann_prix >= :annPrixMin')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('annPrixMin', $resultSearchData['prix_min'])
             ;
         } 
         if ($resultSearchData['prix_max']) {
             $queryBuilder
             ->andWhere('a.ann_prix <= :annPrixMax')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('annPrixMax', $resultSearchData['prix_max']) 
             ;
         }
         if ($resultSearchData['km_min']) {
             $queryBuilder
             ->andWhere('a.kilometre >= :kilometreMin')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('kilometreMin', $resultSearchData['km_min'])
             ;
         }
         if ($resultSearchData['km_max']) {
             $queryBuilder
             ->andWhere('a.kilometre <= :kilometreMax')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('kilometreMax', $resultSearchData['km_max'])
             ;
         }
         if ($resultSearchData['annee']) {
             $queryBuilder
             ->andWhere('a.annee_modele like :anneeModele')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('anneeModele', $resultSearchData['annee'])
             ;
         }
         if ($resultSearchData['boite_de_vitesse']) {
             $queryBuilder
             ->andWhere('a.boite_de_vitesse like :boiteDeVitesse')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('boiteDeVitesse', $resultSearchData['boite_de_vitesse'])
             ;
         }
         if ($resultSearchData['carburant']) {
             $queryBuilder
             ->andWhere('a.carburant like :carburant')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('carburant', $resultSearchData['carburant'])
             ;
         }
         if ($resultSearchData['region']) {
             $queryBuilder
             ->andWhere('a.region like :region')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('region', $resultSearchData['region'])
             ;
         }
         if ($resultSearchData['departement']) {
             $queryBuilder
             ->andWhere('a.departement like :departement')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('departement', $resultSearchData['departement'])
             ;
         }
         if ($resultSearchData['ville']) {
             $queryBuilder
             ->andWhere('a.ville like :ville')
+            ->andWhere('a.ann_a_valider = 0 and a.ann_signaler = 0 and a.ann_active = 1')
             ->setParameter('ville', $resultSearchData['ville'])
             ;
         }
