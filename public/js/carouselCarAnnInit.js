10650e863dd9a54c7562275991004dbd50c3f6e1
florent11 <florentvila@hotmail.fr>
2020-05-18 17:02:11 +0200

    Correction pb mise en forme CSS ".userpanel-container"; Correction pb du code flexslider (load); Correction erreur balisage <img> dans ann-article; Changement de la version de jQuery (CDN); Indentation et création d'un container-background pour "contact_mail" et "contact_mail_annonceur".

--- public/js/carouselCarAnnInit.js
+++ public/js/carouselCarAnnInit.js
@@ -2,7 +2,7 @@ class carouselCarAnnInit
 {
   flexSliderInit()
   {
-    $(window).load(function() {
+    $(window).on('load', function() {
       // The slider being synced must be initialized first
       $('#carousel').flexslider({
         animation: "slide",
